3c1321187c760e3ece09524a4ede7cdb741afbb5
Arthur <arthh.arte.mad046@gmail.com>
2025-06-02 23:05:28 -0300

    Home section

--- projeto/src/App.jsx
+++ projeto/src/App.jsx
@@ -1,13 +1,18 @@
 import './assets/css/style.css'
 
 import Header from './components/Header/Header';
+import Home from './components/Home/HomeSection';
+import Hydration from './components/Hydration/HydrationSection';
+import News from './components/News/NewsSection';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-
+      <Home />
+      <Hydration />
+      <News />
       </main>
     </>
   );
---
+++ projeto/src/components/home/homeSection.jsx
@@ -0,0 +1,29 @@
+import './Home.css';
+import bannerImg from '../../assets/img/banner.png';
+
+const HomeSection = () => {
+  return (
+    <>
+      <section
+        id="home"
+        className="section home"
+        style={{
+          backgroundImage: `url(${bannerImg})`,
+          backgroundSize: 'contain',
+          height: '100vh'
+        }}
+      >
+        <div className="home-right">
+          <h1>Aqui começa sua jornada para uma vida mais saudável!</h1>
+          <p>
+            Crie sua ficha de treino personalizada e alcance seus objetivos com precisão.
+            Simples, rápido e do seu jeito
+          </p>
+          <button className="btn btn-red">Comece Agora</button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HomeSection;
