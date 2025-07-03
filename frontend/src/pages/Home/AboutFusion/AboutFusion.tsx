7650206ba554aa0c443cce5798d3f7d9a95a4e6d
Arthur <arthh.arte.mad046@gmail.com>
2025-07-03 19:43:29 -0300

    sessão sobre a fusion

---
+++ frontend/src/pages/Home/AboutFusion/AboutFusion.tsx
@@ -0,0 +1,19 @@
+import {
+  AboutContainer,
+  Title,
+  Description,
+  ReadMoreButton
+} from './AboutFusion'
+
+export const AboutFusion = () => {
+  return (
+    <AboutContainer>
+      <Title>Sobre a Fusion</Title>
+      <Description>
+        Acompanhe sua ingestão diária de água com nosso contador inteligente.
+        Beba a quantidade certa, alcance suas metas e garanta mais saúde e desempenho nos treinos!
+      </Description>
+      <ReadMoreButton>Ler mais</ReadMoreButton>
+    </AboutContainer>
+  )
+}
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Header } from '../../components/Header/Header.tsx'
 import { HeroSection } from './HeroSection/Hero.tsx'
+import { AboutFusion } from './AboutFusion/AboutFusion.tsx' 
 
 export const Home = () => {
   return (
     <>
       <Header />
       <HeroSection />
+      <AboutFusion />
     </>
   )
 }
